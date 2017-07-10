8ef0d61083ace297a39fe7e7a10e717d934d8d25
Viet Nguyen <viet_nguyen@outlook.com>
2017-07-10 16:06:49 +0000

    commit

---
+++ BraddenWebApp/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+using BraddenWebApp.Models;
+
+namespace BraddenWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppSettings _appSettings;
+
+        public HomeController(IOptions<AppSettings> appSettings)
+        {
+            _appSettings = appSettings.Value;
+        }
+        public IActionResult Index()
+        {
+            string region = _appSettings.Region;
+
+            ViewData.Add("region", region);
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
